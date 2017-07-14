1d15cdd8dc4d9d60fbf7e492779095abec20ca3c
Kurt Schneider <k.w.schneid@gmail.com>
2017-07-13 20:08:40 -0400

    styling changes

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,18 +1,18 @@
 // FIXED SIDE BAR AFTER 100VH
 
 // Retrieve height of screen, scroll position, and column element
-let h = window.innerHeight;
-let scrollPos = 0;
-let leftColumn = document.getElementById('leftColumn');
+// let h = window.innerHeight;
+// let scrollPos = 0;
+// let leftColumn = document.getElementById('leftColumn');
 
-window.addEventListener('scroll', (e) => {
-  scrollPos = window.scrollY;
-  if ( (scrollPos - h) < 0 ) {
-  	leftColumn.style.position = 'absolute';
-  } else {
-  	leftColumn.style.position = 'fixed';
-  }
-});
+// window.addEventListener('scroll', (e) => {
+//   scrollPos = window.scrollY;
+//   if ( (scrollPos - h) < 0 ) {
+//   	leftColumn.style.position = 'absolute';
+//   } else {
+//   	leftColumn.style.position = 'fixed';
+//   }
+// });
 
 // HIDDEN CLASS
 
@@ -39,13 +39,40 @@ window.addEventListener('resize', (e) => {
 
 // Burger Menu hide and show
 
-// Get Burger Menu UL
+// Get Burger Menu UL, pageTitle element, second burger line
 
 let burgerMenu = document.getElementById('burgerMenu');
+let pageTitle = document.getElementById('pageTitle');
+let burgerLineTwo = document.getElementById('burgerLineTwo');
+let burgerLineOne = document.getElementById('burgerLineOne');
+let burgerLineThree = document.getElementById('burgerLineThree');
 
-// Event listener to toggle hidden class for burger click
 burger.addEventListener('click', (e) => {
+	// Event listener to toggle hidden class for burger click
 	burgerMenu.classList.toggle('hidden');
+
+	// if statement to hide .pageTitle on click and display proper menu
+	if (burgerMenu.classList.contains('hidden')) {
+		pageTitle.classList.remove('vHidden');
+
+		// Toggle animation for burger click
+		burger.classList.remove('open');
+
+		// Animate X button for burger
+		// burgerLineTwo.classList.remove('hidden');
+		// burgerLineOne.classList.remove('burgerXOne');
+		// burgerLineThree.classList.remove('burgerXThree');
+	} else {
+		pageTitle.classList.add('vHidden');
+
+		// Toggle animation for burger click
+		burger.classList.add('open');
+
+		// Animate X button for burger
+		// burgerLineTwo.classList.add('hidden');
+		// burgerLineOne.classList.add('burgerXOne');
+		// burgerLineThree.classList.add('burgerXThree');
+	}
 });
 
 // Set hidden classes on page load, if window is smaller than 768px
@@ -55,4 +82,27 @@ var w = window.innerWidth;
 if ( w < 768 ) {
 	burger.classList.remove("hidden");
 	burgerMenu.classList.add("hidden");
+}
+
+// SVG
+
+// Get SVG's and LI
+
+let home = document.getElementById('home');
+let homeHover = document.getElementById('homeHover');
+let homeSVG = document.getElementById('homeSVG');
+
+// Attach Event Listener 
+homeSVG.addEventListener('mouseover', homeOver);
+homeSVG.addEventListener('mouseout', homeOut);
+
+// run hover function and hoverout function
+function homeOver(x) {
+	home.style.display = 'none';
+	homeHover.style.display = 'block';
+}
+
+function homeOut(x) {
+	homeHover.style.display = 'none';
+	home.style.display = 'block';
 }
