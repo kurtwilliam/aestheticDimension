a9d2dddd52c1750b7890c94d0248e88118aebd59
Kurt Schneider <k.w.schneid@gmail.com>
2017-07-25 09:45:36 -0400

    Trying to get home page working

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,156 +1,4 @@
- // Check to see if on home page
-
-document.addEventListener('DOMContentLoaded', function(){
-	let aboutTag = document.getElementById('aboutTag');
-  if ( aboutTag ) { //if the element exists add the click event
-  	
-
-// CHANGE LANDING PAGE ON HOVER!
-
-// Get list items to hover 
-let projectsTag = document.getElementById('projectsTag');
-let blogTag = document.getElementById('blogTag');
-let contactTag = document.getElementById('contactTag');
-
-// Get SVGs
-let initYellow = document.getElementById('initYellow');
-let bgYellow = document.getElementById('bgYellow');
-let hoverYellow = document.getElementById('hoverYellow');
-let initRed = document.getElementById('initRed');
-let bgRed = document.getElementById('bgRed');
-let hoverRed = document.getElementById('hoverRed');
-let initGreen = document.getElementById('initGreen');
-let bgGreen = document.getElementById('bgGreen');
-let hoverGreen = document.getElementById('hoverGreen');
-let initBlue = document.getElementById('initBlue');
-let bgBlue = document.getElementById('bgBlue');
-let hoverBlue = document.getElementById('hoverBlue');
-
-// get red div for zIndex
-let redDiv = document.getElementById('red');
-
-// Get header element to change BG of on hover
-// let header = document.getElementById('header');
-
-// Add event listeners to each a tag
-aboutTag.addEventListener('mouseout', hoverOut);
-projectsTag.addEventListener('mouseout', hoverOut);
-blogTag.addEventListener('mouseout', hoverOut);
-contactTag.addEventListener('mouseout', hoverOut);
-
-// About Tag yellow event listener for hover
-aboutTag.onmouseover = function () {
-	initYellow.classList.remove("hidden");
-	initRed.classList.add("hidden");
-	initGreen.classList.add("hidden");
-	initBlue.classList.add("hidden");
-
-	bgYellow.classList.add("hidden");
-	bgRed.classList.remove("hidden");
-	bgGreen.classList.remove("hidden");
-	bgBlue.classList.remove("hidden");
-
-	initYellow.style.filter = "drop-shadow(5px 9px 6px rgba(67,66,66,.56))"
-	hoverRed.classList.add("hidden");
-	hoverGreen.classList.add("hidden");
-	hoverBlue.classList.add("hidden");
-
-	redDiv.style.zIndex = '-100';
-
-	// Add hollow text class to other tags
-	projectsTag.classList.add('hollowText');
-	blogTag.classList.add('hollowText');
-	contactTag.classList.add('hollowText');
-}
-
-// projects tag red event listener for hover
-projectsTag.onmouseover = function () {
-	initYellow.classList.add("hidden");
-	initRed.classList.add("hidden");
-	initGreen.classList.add("hidden");
-	initBlue.classList.add("hidden");
-
-	bgYellow.classList.remove("hidden");
-	bgRed.classList.add("hidden");
-	bgGreen.classList.remove("hidden");
-	bgBlue.classList.remove("hidden");
-
-	hoverRed.classList.remove("hidden");
-	hoverGreen.classList.add("hidden");
-	hoverBlue.classList.add("hidden");
-
-	aboutTag.classList.add('hollowText');
-	blogTag.classList.add('hollowText');
-	contactTag.classList.add('hollowText');
-}
-
-// blog tag blue event listener for hover
-blogTag.onmouseover = function () {
-	initYellow.classList.add("hidden");
-	initRed.classList.add("hidden");
-	initGreen.classList.add("hidden");
-	initBlue.classList.add("hidden");
-
-	bgYellow.classList.remove("hidden");
-	bgRed.classList.remove("hidden");
-	bgGreen.classList.remove("hidden");
-	bgBlue.classList.add("hidden");
-
-	hoverRed.classList.add("hidden");
-	hoverGreen.classList.add("hidden");
-	hoverBlue.classList.remove("hidden");
-
-	projectsTag.classList.add('hollowText');
-	aboutTag.classList.add('hollowText');
-	contactTag.classList.add('hollowText');
-}
-
-// contact tag green event listener for hover
-contactTag.onmouseover = function () {
-	initYellow.classList.add("hidden");
-	initRed.classList.add("hidden");
-	initGreen.classList.add("hidden");
-	initBlue.classList.add("hidden");
-
-	bgYellow.classList.remove("hidden");
-	bgRed.classList.remove("hidden");
-	bgGreen.classList.add("hidden");
-	bgBlue.classList.remove("hidden");
-
-	hoverRed.classList.add("hidden");
-	hoverBlue.classList.add("hidden");
-	hoverGreen.classList.remove("hidden");
-
-	projectsTag.classList.add('hollowText');
-	blogTag.classList.add('hollowText');
-	aboutTag.classList.add('hollowText');
-}
-
-// Revert BG to normal
-function hoverOut(x) {
-	initYellow.classList.remove("hidden");
-	bgYellow.classList.add("hidden");
-	initRed.classList.remove("hidden");
-	bgRed.classList.add("hidden");
-	hoverRed.classList.add("hidden");
-	initGreen.classList.remove("hidden");
-	bgGreen.classList.add("hidden");
-	hoverGreen.classList.add("hidden");
-	initBlue.classList.remove("hidden");
-	bgBlue.classList.add("hidden");
-	hoverBlue.classList.add("hidden");
-
-	redDiv.style.zIndex = '1';
-	initYellow.style.filter = "drop-shadow(0 0 0 rgba(0,0,0,0))"
-
-	// Remove hollow text on hover out
-	aboutTag.classList.remove('hollowText');
-	projectsTag.classList.remove('hollowText');
-	blogTag.classList.remove('hollowText');
-	contactTag.classList.remove('hollowText');
-};
-
-};
+ // Ch
 
 // Home SVG
 
@@ -214,29 +62,16 @@ if ( form ) {
 	} 
 };
 
-});
-
-// Burger menu toggler
-
-// Set visibility of burger and menu on page load
-
-// if ( w < 768 ) {
-// 	burger.classList.remove("vHidden");
-// 	burger.classList.add('mobileBurger');
-// } else {
-// 	burger.classList.add("vHidden");
-// }
 
 burger.addEventListener('click', (e) => {
+	console.log('hi')
 	burger.classList.toggle('open')
 });
 
 // never even runs...
 function windowResize() {
 	if (window.matchMedia("(min-width: 769px)").matches) {	
-		// console.log('hi')
-		// burger.classList.remove("vHidden");
-	// less than 769px wide
+	
 	} else {
 		burger.classList.remove("vHidden");
 		burger.addEventListener('click', (e) => {
@@ -257,6 +92,7 @@ var w = window.innerWidth;
 
 function resize() { 
 	w = window.innerWidth;
+
 	let leftColumn = document.getElementById('leftColumn');
 
 	if ( w < 768 ) {
@@ -317,6 +153,8 @@ function testScroll(){
 	let rightColumn = document.getElementById('rightColumn');
 	let burgerMenu = document.getElementById('burgerMenu');
 	let responsiveTitle = document.getElementById('responsiveTitle');
+	let socialMedia = document.getElementById('socialMedia');
+
 
 	// If screen is wider than 768px run if statement below
 	if (window.matchMedia("(min-width: 769px)").matches) {
@@ -338,7 +176,8 @@ function testScroll(){
 			}
 		});	
 		// if the scroll is lower than 250px from top
-		if(window.pageYOffset>250) {
+
+		if(window.pageYOffset>250 && socialMedia) {
 			// if the scroll is lower than 250px from top move left sidebar over and make skinny, and make right sidebar wider, and add burger menu
 			leftColumn.classList.add('under300L');
 			rightColumn.classList.add('under300R');
